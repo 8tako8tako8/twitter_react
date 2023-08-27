e28b5391180f8c1f8a3100e7b16959fc8d35b9d2
8tako8tako8 <8tako8tako8@gmail.com>
2023-08-27 23:33:41 +0900

    ・登録後のメッセージ表示 ・エラーメッセージの表示 ・confirmSuccessURLの追加

--- src/components/pages/SignUp.tsx
+++ src/components/pages/SignUp.tsx
@@ -2,77 +2,89 @@
 /* eslint-disable @typescript-eslint/no-unsafe-argument */
 /* eslint-disable @typescript-eslint/no-unsafe-member-access */
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
-import React from 'react'
+import React, { useState } from 'react'
 import styled from 'styled-components'
 import { signUp } from '../../lib/api/auth'
 import { SignUpParams } from '../../types'
+import { ErrorMessage } from '../organisms/ErrorMessage'
 
 export const SignUp: React.FC = () => {
-  const [email, setEmail] = React.useState('')
-  const [password, setPassword] = React.useState('')
-  const [passwordConfirmation, setPasswordConfirmation] = React.useState('')
-
-  const homeUrl = process.env.PUBLIC_URL
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [passwordConfirmation, setPasswordConfirmation] = useState('')
+  const [isSignUp, setIsSignUp] = useState(false)
+  const [errorMessage, setErrorMessage] = useState('')
 
   const handleSignUp = (e: React.FormEvent) => {
     e.preventDefault()
 
-    console.log(email, password, passwordConfirmation)
+    const confirmSuccessURL =
+      process.env.NODE_ENV === 'production'
+        ? (process.env.REACT_APP_CONFIRM_SUCCESS_URL_PROD as string)
+        : (process.env.REACT_APP_CONFIRM_SUCCESS_URL_LOCAL as string)
 
     const params: SignUpParams = {
       email: email,
       password: password,
       passwordConfirmation: passwordConfirmation,
-      confirmSuccessURL: homeUrl + '/confirm',
+      confirmSuccessURL: confirmSuccessURL,
     }
 
     // eslint-disable-next-line @typescript-eslint/no-unsafe-call
     signUp(params)
       .then((res) => {
         if (res.status === 200) {
-          console.log('success')
+          setIsSignUp(true)
+          setErrorMessage('')
         } else {
-          console.log('failed')
+          setErrorMessage('登録に失敗しました')
         }
       })
       .catch((err) => {
-        console.error(err)
+        setErrorMessage('メールアドレス または パスワード が正しくありません')
       })
   }
 
   return (
     <StyledSignUp>
       <h1>アカウント登録</h1>
-      <form onSubmit={handleSignUp}>
-        <div className="inputArea">
-          <label htmlFor="email">メールアドレス:</label>
-          <input
-            id="email"
-            type="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          />
-        </div>
-        <div className="inputArea">
-          <label htmlFor="password">パスワード:</label>
-          <input
-            id="password"
-            type="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-        </div>
-        <div className="inputArea">
-          <label htmlFor="password">パスワード確認:</label>
-          <input
-            id="passwordConfirmation"
-            type="passwordConfirmation"
-            value={passwordConfirmation}
-            onChange={(e) => setPasswordConfirmation(e.target.value)}
-          />
-        </div>
-        <button type="submit">登録</button>
-      </form>
+      {!isSignUp && (
+        <form onSubmit={handleSignUp}>
+          <div className="inputArea">
+            <label htmlFor="email">メールアドレス:</label>
+            <input
+              id="email"
+              type="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </div>
+          <div className="inputArea">
+            <label htmlFor="password">パスワード:</label>
+            <input
+              id="password"
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </div>
+          <div className="inputArea">
+            <label htmlFor="password">パスワード確認:</label>
+            <input
+              id="passwordConfirmation"
+              type="passwordConfirmation"
+              value={passwordConfirmation}
+              onChange={(e) => setPasswordConfirmation(e.target.value)}
+            />
+          </div>
+          <button type="submit">登録</button>
+        </form>
+      )}
+
+      {isSignUp && (
+        <p>ご登録ありがとうございます。メールを確認してください。</p>
+      )}
+      {errorMessage && <ErrorMessage>{errorMessage}</ErrorMessage>}
     </StyledSignUp>
   )
 }
