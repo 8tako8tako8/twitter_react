02fcc896ad809dc24c2635229ad460cb8ce3a935
8tako8tako8 <8tako8tako8@gmail.com>
2023-09-03 23:04:54 +0900

    認証済みユーザーをAPIから取得したい

--- src/lib/api/auth.ts
+++ src/lib/api/auth.ts
@@ -1,3 +1,5 @@
+import Cookies from 'js-cookie'
+
 import client from './client'
 import { SignUpParams, LoginParams } from '../../types/index'
 
@@ -9,4 +11,23 @@ export const signUp = (params: SignUpParams) => {
 // ログイン
 export const login = (params: LoginParams) => {
   return client.post('users/sign_in', params)
+}
+
+// 認証済みのユーザーを取得
+export const getCurrentUser = () => {
+  if (
+    !Cookies.get('_access_token') ||
+    !Cookies.get('_client') ||
+    !Cookies.get('_uid')
+  )
+    return
+
+  // prettier-ignore
+  return client.get('/auth/sessions', {
+    headers: {
+      "access-token": Cookies.get("_access_token"),
+      "client": Cookies.get("_client"),
+      "uid": Cookies.get("_uid")
+    },
+  })
 }
