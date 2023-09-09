28ab43f73e0f247556e4465544cdb096b83dc3f5
8tako8tako8 <8tako8tako8@gmail.com>
2023-09-09 16:08:48 +0900

    ツイートフォームとツイート一覧を組み合わせてタイムラインのテンプレートを作成したい

---
+++ src/components/templates/TimeLine.tsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import styled from 'styled-components'
+import { TweetBox } from '../organisms/TweetBox'
+import { Posts } from '../organisms/Posts'
+
+export const TimeLine: React.FC = () => {
+  return (
+    <StyledTimeLine>
+      <div className="timeLineHeader">
+        <h2>ホーム</h2>
+      </div>
+      <TweetBox />
+      <Posts />
+    </StyledTimeLine>
+  )
+}
+
+const StyledTimeLine = styled.div`
+  .timeLineHeader {
+    position: sticky;
+    top: 0;
+    background-color: white;
+    z-index: 100;
+    border: 1px solid var(--twitter-background);
+    padding: 5px 20px;
+  }
+
+  .timeLineHeader > h2 {
+    font-size: 20px;
+    font-weight: 800;
+  }
+`
