fb5449ee3ea845a4b582e1184b7007dfe0452675
8tako8tako8 <8tako8tako8@gmail.com>
2023-09-19 14:21:34 +0900

    コメント一覧にページネーションを追加したい

--- src/components/organisms/Comments.tsx
+++ src/components/organisms/Comments.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { styled } from 'styled-components'
 import { Comment } from './Comment'
+import { Pagination } from '@mui/material'
 
 type Comment = {
   id: number
@@ -42,13 +43,33 @@ const initialComments: Comment[] = [
 export const Comments: React.FC = () => {
   const [comments] = React.useState<Comment[]>(initialComments)
 
+  const totalPages = 1
+  const currentPage = 1
+
   return (
     <StyledComments>
       {comments.map((comment) => (
         <Comment key={comment.id} comment={comment} />
       ))}
+      <div className="pagination">
+        <Pagination
+          count={totalPages}
+          page={currentPage}
+          variant="outlined"
+          color="primary"
+          size="small"
+        />
+      </div>
     </StyledComments>
   )
 }
 
-const StyledComments = styled.div``
+const StyledComments = styled.div`
+  .pagination {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    margin-top: 20px;
+    margin-bottom: 20px;
+  }
+`
